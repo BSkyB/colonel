a574af5a994e29069698884f060e677b2fe7947c
Viktor Charypar <charypar@gmail.com>
2014-02-27 11:24:48 +0000

    Added test coverage using simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.start do
+    coverage_dir(dir)
+  end
+else
+  SimpleCov.start
+end
+
 require 'git_cma'
 include GitCma
 
