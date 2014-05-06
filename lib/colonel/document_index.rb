4c47dcfbd74959f258c4d426d98b71f527b390a7
Viktor Charypar <charypar@gmail.com>
2014-05-06 14:10:15 +0100

    Added document index

---
+++ lib/colonel/document_index.rb
@@ -0,0 +1,62 @@
+module Colonel
+  # Public: Document index holding the list of all documents in a given `storage_path`. This is a low-leve
+  # document manifest used for maintenance - e.g. backup and restore, or reindexing in elasticsearch.
+  # You should only use this class if you're not using the ContentItem at all or for Colonel-level tools.
+  #
+  # Internally the index is a file in a git repo-like storage: A single ref to a blob object that gets
+  # updated every time the index changes.
+  class DocumentIndex
+    INDEX_NAME = 'colonel/document-index'
+
+    attr_reader :storage_path
+
+    # Public: Get an index for a given `storage_path`, where documents are stored. You almost never
+    # need to do this directly, `Document` does it for you.
+    def initialize(storage_path)
+      @storage_path = storage_path
+    end
+
+    # Public: List all the documents this index is aware of.
+    #
+    # returns an array of document names as strings
+    def documents
+      @documents if @documents
+
+      begin
+        rev = repository.head.target_id
+      rescue Rugged::ReferenceError
+        return []
+      end
+
+      file = repository.lookup(rev).read_raw.data
+      @documents = file.split("\n")
+    end
+
+    # Public: Idempotently register a document. If the document isn't yet known, it will be added to the
+    # index.
+    def register(document_name)
+      # FIXME - this check is potentially sloooow. We may need a Hash or a trie-like datastructure
+      return true if documents.include?(document_name)
+
+      docs = (documents + [document_name]).join("\n")
+      oid = repository.write(docs, :blob)
+
+      if ref = repository.references["refs/heads/master"]
+        repository.references.update(ref, oid)
+      else
+        repository.references.create("refs/heads/master", oid)
+      end
+
+      true
+    end
+
+    # Internal: repository for the index
+    def repository
+      unless Colonel.config.rugged_backend.nil?
+        @repo ||= Rugged::Repository.init_at(File.join(@storage_path, INDEX_NAME), :bare, backend: Colonel.config.rugged_backend)
+      else
+        @repo ||= Rugged::Repository.init_at(File.join(@storage_path, INDEX_NAME), :bare)
+      end
+    end
+  end
+end
---
+++ spec/document_index_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe DocumentIndex do
+  it "should have a name" do
+    expect(DocumentIndex::INDEX_NAME).to eq('colonel/document-index')
+  end
+
+  it "should initialize with storage path" do
+    expect { DocumentIndex.new('test-storage') }.not_to raise_error()
+  end
+
+  it "should have a repository" do
+    index = DocumentIndex.new('test-storage')
+
+    expect(index.repository).to be_a(Rugged::Repository)
+  end
+
+  describe "Indexing and retrieving" do
+    let :repo do
+      Struct.new(:references).new([])
+    end
+
+    let :head do
+      Struct.new(:target_id).new("abcdef")
+    end
+
+    let :ref do
+      Object.new
+    end
+
+    let :object do
+      Object.new
+    end
+
+    let :raw do
+      Object.new
+    end
+
+    let :index do
+      index = DocumentIndex.new('test-storage')
+      allow(index).to receive(:repository).and_return(repo)
+
+      index
+    end
+
+    it "should load the documents list for empty repo" do
+      expect(repo).to receive(:head).and_raise(Rugged::ReferenceError)
+
+      expect(index.documents).to be_a(Array)
+    end
+
+    it "should load the documents list for non-empty repo" do
+      expect(repo).to receive(:head).and_return(head)
+      expect(repo).to receive(:lookup).with('abcdef').and_return(object)
+      expect(object).to receive(:read_raw).and_return(raw)
+      expect(raw).to receive(:data).and_return("one\ntwo\nthree")
+
+      expect(index.documents).to eq(['one', 'two', 'three'])
+    end
+
+    it "should register a document in an empty repo" do
+      allow(index).to receive(:documents).and_return(["one", "two", "three"])
+      expect(repo).to receive(:write).with("one\ntwo\nthree\nfour", :blob).and_return('aabbccdd')
+
+      expect(repo.references).to receive(:[]).with('refs/heads/master').and_return(nil)
+      expect(repo.references).to receive(:create).with('refs/heads/master', 'aabbccdd')
+
+      expect(index.register("four")).to eq(true)
+    end
+
+    it "should register a document in an non-empty repo" do
+      allow(index).to receive(:documents).and_return(["one", "two", "three"])
+      expect(repo).to receive(:write).with("one\ntwo\nthree\nfour", :blob).and_return('aabbccdd')
+
+      expect(repo.references).to receive(:[]).with('refs/heads/master').and_return(ref)
+      expect(repo.references).to receive(:update).with(ref, 'aabbccdd')
+
+      expect(index.register("four")).to eq(true)
+    end
+
+    it "should not modify a repo with document already listed" do
+      allow(index).to receive(:documents).and_return(["one", "two", "three", "four"])
+
+      expect(repo).not_to receive(:write)
+      expect(repo.references).not_to receive(:[])
+      expect(repo.references).not_to receive(:update)
+
+      expect(index.register("four")).to eq(true)
+    end
+  end
+end
